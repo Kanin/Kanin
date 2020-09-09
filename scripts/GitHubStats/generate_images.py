19cd345f63dde24c2e413b04484a13be22f9abca
Kanin <im@kanin.dev>
2020-09-09 00:54:10 -0400

    Progress?

--- scripts/GitHubStats/generate_images.py
+++ scripts/GitHubStats/generate_images.py
@@ -30,7 +30,7 @@ async def generate_overview(s: Stats) -> None:
     Generate an SVG badge with summary statistics
     :param s: Represents user's GitHub statistics
     """
-    with open("templates/overview.svg", "r") as f:
+    with open("scripts/GitHubStats/templates/overview.svg", "r") as f:
         output = f.read()
 
     output = re.sub("{{ name }}", await s.name, output)
@@ -53,7 +53,7 @@ async def generate_languages(s: Stats) -> None:
     Generate an SVG badge with summary languages used
     :param s: Represents user's GitHub statistics
     """
-    with open("templates/languages.svg", "r") as f:
+    with open("scripts/GitHubStats/templates/languages.svg", "r") as f:
         output = f.read()
 
     progress = ""
