b7f667b7c7e5f411c1de331c6731779afe36b800
Kanin <im@kanin.dev>
2020-09-09 00:57:47 -0400

    Lets save them here just so we know

--- scripts/GitHubStats/generate_images.py
+++ scripts/GitHubStats/generate_images.py
@@ -44,7 +44,7 @@ async def generate_overview(s: Stats) -> None:
     output = re.sub("{{ repos }}", f"{len(await s.repos):,}", output)
 
     generate_output_folder()
-    with open("generated/overview.svg", "w") as f:
+    with open("scripts/GitHubStats/generated/overview.svg", "w") as f:
         f.write(output)
 
 
@@ -81,7 +81,7 @@ fill-rule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8z"></path></svg>
     output = re.sub(r"{{ lang_list }}", lang_list, output)
 
     generate_output_folder()
-    with open("generated/languages.svg", "w") as f:
+    with open("scripts/GitHubStats/generated/languages.svg", "w") as f:
         f.write(output)
 
 
